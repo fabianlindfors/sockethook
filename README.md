785256435d6c653bbf368062beab484f8af0bf42
Fabian Lindfors <fabian@flapplabs.se>
2018-06-14 20:39:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Sockethook
+
+Sockethook is a Webhook->Websocket proxy written in Go. It's designed for building real-time applications around third-party APIs which provide Webhooks. Sockethook could for example be used to create a live feed of [Github](https://developer.github.com/webhooks/) deployments or a real-time view of [Shopify](https://help.shopify.com/api/reference/events/webhook) orders.
+
+## Usage
+
+Sockethook is written in Go and can be installed by running
+
+`$ go get github.com/fabianlindfors/sockethook`
+
+The tool is then started with
+
+```
+$ sockethook
+INFO[0000] Sockethook is ready and listening ✅
+```
+
+Sockethook is now ready to start receiving Webhooks! Websockets can be connected through `/socket` followed by the endpoint to which you want to subscribe, for example: `/socket/order/created`. Any Webhook requests sent to `/hook/order/created` will now be broadcast to all subscribers listening to the specific endpoint.
+
+The broadcasted message will be JSON encoded and contain information about the Webhook request. The following is an example message from a Shopify Webhook:
+
+```json
+{
+  "headers": {
+    "Accept": "*\/*",
+    "Accept-Encoding": "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+    "Connection": "close",
+    "Content-Length": "4264",
+    "Content-Type": "application\/json",
+    "User-Agent": "Ruby",
+    "X-Forwarded-For": "35.231.14.37",
+    "X-Forwarded-Proto": "https",
+    "X-Shopify-Hmac-Sha256": "wa5ZVAJPjbtr4Oj8xVnt\/jLWwfD9JdGFcdrjY4VgORQ=",
+    "X-Shopify-Order-Id": "820982911946154508",
+    "X-Shopify-Shop-Domain": "fabianlindfors.myshopify.com",
+    "X-Shopify-Test": "true",
+    "X-Shopify-Topic": "orders\/create"
+  },
+  "endpoint": "\/order\/created",
+  "data": {
+    "app_id": null,
+    "billing_address": {
+      "address1": "123 Billing Street",
+      "address2": null,
+      "city": "Billtown",
+      "company": "My Company",
+      "country": "United States",
+      "country_code": "US",
+      "first_name": "Bob",
+      "last_name": "Biller",
+      "latitude": null,
+      "longitude": null,
+      "name": "Bob Biller",
+      "phone": "555-555-BILL",
+      "province": "Kentucky",
+      "province_code": "KY",
+      "zip": "K2P0B0"
+    }
+    ...
+  }
+}
+```
+
+If the request content type is JSON then the `data` field will contain the JSON body. Otherwise `data` will be a string of the body.
+
+## Command-line options
+
+Two possible options can be passed to Sockethook, `--port` and `--address`. `--port` specifies which port at which to listen (default is 1234) and `--address` sets a specific address to bind to.
+
+```
+$ sockethook --port 8000
+$ sockethook --address 127.0.0.1
+```
+
+## License
+
+Sockethook is licensed under [MIT](https://github.com/fabianlindfors/sockethook/blob/master/LICENSE).
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"strings"
+)
+
+// Map holding all Websocket clients and the endpoints they are subscribed to
+var clients = make(map[string][]*websocket.Conn)
+var upgrader = websocket.Upgrader{}
+
+// Message which will be sent as JSON to Websocket clients
+type Message struct {
+	Headers  map[string]string `json:"headers"`
+	Endpoint string            `json:"endpoint"`
+	Data     interface{}       `json:"data"`
+}
+
+func handleHook(w http.ResponseWriter, r *http.Request, endpoint string) {
+	msg := Message{}
+	logEntry := log.WithField("endpoint", endpoint)
+
+	// Transfer headers to response
+	msg.Headers = make(map[string]string)
+	for k, v := range r.Header {
+		msg.Headers[k] = v[0]
+	}
+
+	// Set endpoint on response
+	msg.Endpoint = endpoint
+
+	// Read body of request
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+
+	// If request is JSON, unmarshal and save to response. Otherwise just save as string.
+	if r.Header.Get("Content-Type") == "application/json" {
+		json.Unmarshal(buf.Bytes(), &msg.Data)
+	} else {
+		msg.Data = buf.Bytes()
+	}
+
+	// Get all clients listening to the current endpoint
+	conns := clients[endpoint]
+
+	if conns != nil {
+		for i, conn := range conns {
+			if conn.WriteJSON(msg) != nil {
+				// Remove client and close connection if sending failed
+				conns = append(conns[:i], conns[i+1:]...)
+				conn.Close()
+			}
+		}
+	}
+
+	clients[endpoint] = conns
+
+	logEntry.WithField("clients", len(conns)).Infoln("Hook broadcasted")
+}
+
+func handleClient(w http.ResponseWriter, r *http.Request, endpoint string) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	logEntry := log.WithField("endpoint", endpoint)
+
+	if err != nil {
+		logEntry.Println(err)
+		// Send Upgrade required response if upgrade fails
+		w.WriteHeader(426)
+		return
+	}
+
+	// Add client to endpoint slice
+	clients[endpoint] = append(clients[endpoint], conn)
+
+	logEntry.WithField("clients", len(clients[endpoint])).Infoln("Client connected")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimRight(r.URL.Path, "/")
+
+	/**
+	 * Check prefix of URL path:
+	 * 	/hook is used for webhooks and requests will be broadcasted to all listening clients.
+	 * 	/socket is used for connect a new socket client
+	 */
+	if strings.HasPrefix(path, "/hook") {
+		handleHook(w, r, strings.TrimPrefix(path, "/hook"))
+	} else if strings.HasPrefix(path, "/socket") {
+		handleClient(w, r, strings.TrimPrefix(path, "/socket"))
+	} else {
+		log.WithField("path", r.URL.Path).Warnln("404 Not found")
+		w.WriteHeader(404)
+	}
+}
+
+func main() {
+	// Get command line options --address and --port
+	address := flag.String("address", "", "Address to bind to.")
+	port := flag.Int("port", 1234, "Port to bind to. Default: 1234")
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+
+	// Start HTTP server
+	log.Infof("Sockethook is ready and listening at port %d ✅", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", *address, *port), nil))
+}
